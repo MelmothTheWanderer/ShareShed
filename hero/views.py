08dad9da3f49cac11536fb1edde0bf33b0c8022c
Jamie Simms <jamiesimms@protonmail.com>
2024-02-05 23:09:27 +0000

    the base template now has a working header and footer

--- hero/views.py
+++ hero/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 
 def hero_display(request):
-  return render(request, "index.html")
+  current_year = datetime.date.today().year
+  return render(request, "index.html", {
+    "current_year" : current_year
+  })
