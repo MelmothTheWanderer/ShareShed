073c7ce5b1c2f1bad4adf62505bdf9fcb8a7ec71
Jamie Simms <jamiesimms@protonmail.com>
2024-02-06 16:52:00 +0000

    added the  model for the items that are in the shed

--- items/models.py
+++ items/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Item (models.Model):
+    name=models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    image = models.CharField(max_length=200, unique=True)
+    in_stock = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name 
